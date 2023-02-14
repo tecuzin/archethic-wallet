133c7ee9e83346fae797ea9d72009bc3aed9fa06
David ROCHELET <drochelet@groupeisgri.com>
2023-02-14 13:01:37 +0100

    Patrol native configuration and test example

---
+++ android/app/src/main/java/net/archethic/archethic_wallet/MainActivityTest.java
@@ -0,0 +1,12 @@
+package net.archethic.archethic_wallet 
+
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+import pl.leancode.patrol.PatrolTestRule;
+import pl.leancode.patrol.PatrolTestRunner;
+
+@RunWith(PatrolTestRunner.class)
+public class MainActivityTest {
+    @Rule
+    public PatrolTestRule<MainActivity> rule = new PatrolTestRule<>(MainActivity.class);
+}
